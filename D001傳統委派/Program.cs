eb1c41c552389dff598d0a9c0e6f7eda7a211a92
米奇 薛 <Mickey.Shiue@ehsn.com.tw>
2019-07-26 23:16:13 +0800

    1、D001傳統委派Void Init commit

---
+++ D001傳統委派/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D001傳統委派Void
+{
+    //傳統宣告委派方式
+    //缺點很冗長
+    public delegate void MyVoidDelegate(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyVoidDelegate myVoidDelegate = new MyVoidDelegate(MyVoidMethod);
+            myVoidDelegate += MyVoidMethod2;          
+            myVoidDelegate -= MyVoidMethod2;
+            myVoidDelegate.Invoke("Mickey");
+        }
+
+        public static void MyVoidMethod(string name)
+        {
+            Console.WriteLine("Void：" + name);
+        }
+
+        public static void MyVoidMethod2(string name)
+        {
+            Console.WriteLine("Void2:" + name);
+        }
+    }
+}
