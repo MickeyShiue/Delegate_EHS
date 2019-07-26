4151dad2cbb4dcb34e4d827040b5c639b6a7ab3f
米奇 薛 <Mickey.Shiue@ehsn.com.tw>
2019-07-27 01:00:46 +0800

    D004 將委派塞入變數與參數傳遞

---
+++ D004將委派塞入變數與參數傳遞/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D004將委派塞入變數與參數傳遞
+{
+    public delegate void MyDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //變數 myDelegate 是委派，並且註冊了MyMethod 給他
+            MyDelegate myDelegate = new MyDelegate(MyMethod);
+
+            //將委派當作參數，傳遞給InvokeDelegate這個方法，而參數是一個委派，
+            //換句話說也就是傳一個方法給InvokeDelegate
+            InvokeDelegate(myDelegate);
+        }
+
+        public static void MyMethod()
+        {
+            Console.WriteLine("MyMethod");
+        }
+
+        public static void InvokeDelegate(MyDelegate myDelegate)
+        {
+            myDelegate.Invoke();
+        }      
+    }
+}
