36420801f8390725ace246da97f3fb7563d5c759
米奇 薛 <Mickey.Shiue@ehsn.com.tw>
2019-08-04 05:32:28 +0800

    update add d007

---
+++ D007Func介紹/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D007Func介紹
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //基本簡介
+            Func<MemberInfo, bool> filter = MyFilter;
+            var members = GetMembers().Where(MyFilter);
+
+            //示範Select 的型別
+            //members.Select
+
+            //示範OrderBy 的Key
+            //members.OrderBy(r => r);
+        }
+
+        public static bool MyFilter(MemberInfo member)
+        {
+            return member.FistrName == "Mickey";
+        }
+
+        public static IEnumerable<MemberInfo> GetMembers()
+        {
+            List<MemberInfo> members = new List<MemberInfo>()
+            {
+                new MemberInfo{FistrName="Mickey",LastName="Shiue",Gender=true },
+                new MemberInfo{FistrName="Tom",LastName="Ting",Gender=true },
+            };
+            return members;
+        }
+    }
+
+    public class MemberInfo
+    {
+        public string FistrName { get; set; }
+        public string LastName { get; set; }
+        public bool Gender { get; set; }
+    }
+}
