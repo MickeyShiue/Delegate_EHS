3a89410d06a128eb5867bab08672373ca7e7cacc
米奇 薛 <Mickey.Shiue@ehsn.com.tw>
2019-08-04 05:09:53 +0800

    update add

---
+++ D006Predicate介紹/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D006Predicate介紹
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> MyPredicate = MyPredicateMethod;
+
+            List<string> MyLsit = new List<string>()
+            {
+                "Mickey","David","Tom"
+            };
+            MyLsit.Find(r => r.Length > 4);
+        }
+
+        public static bool MyPredicateMethod(string name)
+        {
+            return name.Length > 4;
+        }
+    }
+}
