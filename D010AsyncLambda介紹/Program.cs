6a1c98484627ef1fe54fc293001b9ebc352be1fe
米奇 薛 <Mickey.Shiue@ehsn.com.tw>
2019-08-24 21:02:08 +0800

    updates

--- D010AsyncLambda介紹/Program.cs
+++ D010AsyncLambda介紹/Program.cs
@@ -13,16 +13,18 @@ namespace D010AsyncLambda介紹
         {   
             Console.WriteLine($"Start This Tread ID:{Thread.CurrentThread.ManagedThreadId}");// 1
 
+         
             Task.Run(async () =>
             {
                 await Task.Delay(1000);
                 Console.WriteLine($"Task.Run Thread ID:{Thread.CurrentThread.ManagedThreadId}");//2
             });
-          
+
+            //Thread.Sleep(3000);
             Task.Delay(3000);
             Console.WriteLine($"END");//3
 
-            Console.Read();
+            //Console.Read();
         }
     }
 }
